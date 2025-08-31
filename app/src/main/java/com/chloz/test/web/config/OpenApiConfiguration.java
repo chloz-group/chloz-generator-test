0d8bb0616a26b4e81ffddb43b3662cf13016f377
Gildas AYEFOU <ayefou@guichetunique.org>
2025-08-31 13:34:14 +0100

    Add bearer authentication scheme in the Swagger UI interface

--- app/src/main/java/com/chloz/test/web/config/OpenApiConfiguration.java
+++ app/src/main/java/com/chloz/test/web/config/OpenApiConfiguration.java
@@ -1,10 +1,13 @@
 package com.chloz.test.web.config;
 
 import com.chloz.test.web.Constants;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,8 +21,13 @@ public class OpenApiConfiguration {
 
 	@Bean
 	public OpenAPI springOpenAPI() {
-		return new OpenAPI().info(new Info().title("Chloz test API").description("Chloz test application API")
-				.version("v1.0.0").license(new License().name("Apache 2.0").url("http://www.chloztest.com")));
+		final String securitySchemeName = "bearerAuth";
+		return new OpenAPI()
+				.info(new Info().title("Chloz test API").description("Chloz test application API").version("v1.0.0")
+						.license(new License().name("Apache 2.0").url("http://www.chloztest.com")))
+				.addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+				.components(new Components().addSecuritySchemes(securitySchemeName, new SecurityScheme()
+						.name(securitySchemeName).type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
 	}
 
 	@Bean
